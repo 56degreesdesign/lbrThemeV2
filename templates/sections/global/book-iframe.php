0adf1e73ed6d202d87a23736ac67b8ea59706d56
Radosław Wysocki <radoslawysocki@gmail.com>
2023-11-15 09:44:17 +0100

    Change iframe

--- templates/sections/global/book-iframe.php
+++ templates/sections/global/book-iframe.php
@@ -10,7 +10,12 @@
 <section class="bg-beige pb-16 md:pb-24 lg:pb-32">
     <div class="container grid-layout">
         <div class="col-span-full">
-            <iframe class="w-full" src="https://hotels.cloudbeds.com/reservation/cStQ7x?widget=1" frameborder="0" width="100%" scrolling="no" id="cloudbeds"></iframe>
+            <?php
+                /*
+                 * #room_type=DK - https://myfrontdesk.cloudbeds.com/hc/en-us/articles/115004684433-How-to-redirect-your-guests-to-a-particular-room-type-and-or-rate-plan-in-mybookings-#room-type
+                 */
+            ?>
+            <iframe class="w-full" src="https://hotels.cloudbeds.com/reservation/uWGSeO?widget=1#room_type=DQ;DK" frameborder="0" width="100%" scrolling="no" id="cloudbeds"></iframe>
             <!-- Add script to auto-resize the iFrame -->
             <script type="text/javascript" src="https://hotels.cloudbeds.com/widget/iFrameResizer"></script><script>window.iFrameResize({}, '#cloudbeds')</script>
         </div>
