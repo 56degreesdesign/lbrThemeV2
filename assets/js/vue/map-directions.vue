f07ac25c53a54536e73d8b1475980dab631597b0
Radosław Wysocki <radoslawysocki@gmail.com>
2023-11-20 12:06:52 +0100

    Location - direction map mobile

--- assets/js/vue/map-directions.vue
+++ assets/js/vue/map-directions.vue
@@ -21,7 +21,7 @@ const select = (option) => {
             <div class="col-span-full flex flex-col pt-6 md:pt-10 lg:pt-0 lg:col-span-6 2xl:pt-14">
                 <span v-if="sectionData.heading" class="txt-h4 mb-6">{{sectionData.heading}}</span>
                 <p v-if="sectionData.content" class="text-17 mb-10 lg:mb-14 xl:mb-40 xl:w-1/2" v-html="sectionData.content"></p>
-                <div class="flex gap-x-7 mb-10 md:mb-12 lg:mb-14 xl:mb-20">
+                <div class="flex gap-x-2.5 mb-10 md:mb-12 lg:gap-x-7 lg:mb-14 xl:mb-20">
                     <button @click="select('by_air')"
                             :class="{ 'btn--active': selected === 'by_air', 'btn--inactive': selected !== 'by_air' }"
                             class="btn">by air</button>
@@ -33,11 +33,11 @@ const select = (option) => {
                     <div v-for="item in sectionData.by_air.directions" class="col-span-full md:col-span-1 directions-content border-t" v-html="item.content"></div>
                 </div>
                 <div v-if="selected === 'by_car'" v-for="item in sectionData.by_car.cities" class="flex justify-between py-2 items-center border-t last-of-type:border-b lg:px-7 lg:py-3">
-                    <span class="text-35 font-Reckless uppercase">{{ item.name }}</span>
-                    <span class="text-18 font-bold uppercase">{{ item.time }}</span>
+                    <span class="txt-h3 font-Reckless uppercase">{{ item.name }}</span>
+                    <span class="txt-1 font-bold uppercase">{{ item.time }}</span>
                 </div>
             </div>
-            <div class="col-span-full mt-7 lg:mt-0 lg:col-span-6">
+            <div class="col-span-full mt-7 -order-1 -mx-[25px] lg:order-1 lg:mt-0 lg:col-span-6">
                 <div v-if="sectionData.image" class="relative pb-[133.33%]">
                     <img class="absolute-full object-cover" :src="sectionData.image.url" alt="">
                 </div>
@@ -48,15 +48,15 @@ const select = (option) => {
 
 <style lang="scss" scoped>
 .btn {
-    @apply uppercase px-10 pt-2 pb-1.5 text-15 w-fit font-bold rounded-full hover:opacity-70 duration-300 cursor-pointer lg:w-[162px] leading-1.33;
+    @apply uppercase px-10 pt-2 pb-1.5 text-15 w-full font-bold rounded-full hover:opacity-70 duration-300 cursor-pointer lg:w-[162px] leading-1.33;
     
     &--active {
         @apply bg-orange border border-transparent text-white;
 
     }
+    
     &--inactive {
         @apply border border-orange text-orange;
-
     }
 }
 </style>
--- templates/sections/global/map-and-slider.php
+++ templates/sections/global/map-and-slider.php
@@ -10,15 +10,15 @@ $slides = $section['slides'] ?? false;
         <div class="container grid-layout">
             <div class="col-span-full flex flex-col-reverse lg:flex-col lg:col-start-1 lg:col-span-6">
                 <?php if($image) :?>
-                    <div class="relative pb-[62.1266%] lg:mb-40 xl:mb-52">
+                    <div class="relative pb-[62.1266%] mb-11 -mx-[25px] lg:mb-40 xl:mb-52">
                         <?php get_template_part('templates/partials/images/image', 'bg', ['image' => $image, 'class' => 'w-full']); ?>
                     </div>
                 <?php endif ;?>
                 <?php if($content) : ?>
-                    <div class="map-and-slider-content mb-10 lg:mb-0"> <?php echo $content ?></div>
+                    <div class="map-and-slider-content -order-1"> <?php echo $content ?></div>
                 <?php endif ;?>
             </div>
-            <div class="col-span-full mt-10 lg:col-start-8 lg:col-span-5 lg:mt-32 xl:translate-x-[3.563rem]">
+            <div class="col-span-full mt-11 lg:col-start-8 lg:col-span-5 lg:mt-32 xl:translate-x-[3.563rem]">
                 <div class="swiper small-carousel relative">
                     <div class="swiper-wrapper">
                         <?php foreach ($slides as $slide) : ?>
