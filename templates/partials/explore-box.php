ecd69ba18414a6f3e9492a3b4c0f8f7bdf7e6623
Michalskii <94405347+Michalskii@users.noreply.github.com>
2023-11-21 16:12:36 +0100

    Availability component 1024px+

--- templates/partials/explore-box.php
+++ templates/partials/explore-box.php
@@ -6,7 +6,7 @@ $side = $args['side'] ?? false;
 <div class="col-span-full mb-10 pb-10 lg:flex-col-reverse items-center lg:col-span-1 lg:mb-0 lg:gap-x-10 lg:py-10 hidden lg:flex <?php echo ($side === 'right') ? 'lg:border-l lg:pl-10 justify-end 2xl:flex-row-reverse justify-start' : 'lg:pr-10 2xl:flex-row justify-end' ; ?>">
     <div class="flex flex-col items-center">
         <h3 class="mb-5 txt-h6 md:mb-7 lg:mb-5" ><?php echo $data['heading'] ?></h3>
-        <?php get_template_part( 'templates/partials/button', null, ['data' => $data['button'], 'class' => 'btn--mobile-narrow'] ) ?>
+        <?php get_template_part( 'templates/partials/button', null, ['data' => $data['button'], 'class' => 'btn--mobile-narrow btn--desktop-narrow'] ) ?>
     </div>
     <div class="relative w-full pb-[56%] mb-10 2xl:w-[230px] 2xl:h-[150px] 2xl:pb-0 2xl:mb-0">
         <div class="absolute top-0 left-0 w-full h-full bg-black"></div>
--- templates/sections/global/availability.php
+++ templates/sections/global/availability.php
@@ -18,9 +18,9 @@ $props = json_encode($section, JSON_HEX_APOS);
 <?php if($data && !$interactive) :?> 
 <section class="bg-beige pt-28 pb-16">
     <div class="container grid-layout">
+        <h3 class="col-start-1 col-span-3 txt-h3 mt-6 lg:mb-10 xl:mb-14 xl:col-span-3"><?php echo $data['heading'] ?></h3>
         <div class="col-start-1 col-span-2">
-            <h3 class="txt-h3 mt-6 mb-14"><?php echo $data['heading'] ?></h3>
-            <p class="mb-8"><?php echo $data['subheading'] ?></p>
+            <p class="mb-8 hidden xl:block"><?php echo $data['subheading'] ?></p>
             <div class="flex flex-col gap-y-3">
                 <?php foreach ($data['buttons'] as $button) : ?>
                    <?php get_template_part('templates/partials/button', null, ['data' => $button['button'], 'class' => 'btn--desktop-narrow']); ?>
@@ -28,7 +28,7 @@ $props = json_encode($section, JSON_HEX_APOS);
             </div>
             <img class="mt-24 max-w-[212px]" src="<?php echo $section['legend']['url']?>" alt="">
         </div>
-        <div class="col-start-4 col-span-9">
+        <div class="col-start-4 col-span-9 2xl:row-start-1 2xl:row-span-2">
             <?php get_template_part('templates/partials/map-phase-all'); ?>
         </div>
     </div>
